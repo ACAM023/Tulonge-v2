86e40f093cdeb8e1683de672164954bd3c5b8af6
Francis <frathoso@hotmail.com>
2019-02-16 23:26:00 -0800

    Add chat library script

---
+++ db/db_libs.php
@@ -0,0 +1,21 @@
+<?PHP
+/**
+ *	@author 	Francis Sowani (Frathoso) <frathoso@gmail.com>
+ *	@author 	Alvin Mrema <sonalpha023@gmail.com>
+ *	@version	1.0
+ */
+
+
+/*	Requires necesssary scripts  */
+require_once (__DIR__.'/db_connection.php');
+
+
+/**
+ *	Defines chats related functions
+ */
+
+// TODO
+
+echo "TODO";
+
+?>
