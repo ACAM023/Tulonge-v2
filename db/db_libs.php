cb7fc66588b822fef7e33b313ecbac3891de9f0f
ACAM023 <sonalpha023@gmail.com>
2019-02-22 17:36:30 +0300

    Completed

--- db/db_libs.php
+++ db/db_libs.php
@@ -14,8 +14,25 @@ require_once (__DIR__.'/db_connection.php');
  *	Defines chats related functions
  */
 
+function selectAllChats(){
+   global $conn, $error;
+
+   $chatList = null;
+   if ($conn != null){
+       $query = "SELECT message, sent_time FROM chats";
+       $statement = $conn->prepare($query);
+       $statement->execute();
+
+       $chatList = $statement->fetchAll(PDO::FETCH_ASSOC);
+     }else {
+       echo "Error (selectAllChats): ".$error->getMessage();
+     }
+
+   return $chatList;
+ }
+
 // TODO
 
-echo "TODO";
+//echo "TODO";
 
 ?>
