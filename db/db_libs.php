41d358da4935d4d2b3ae2c701f961d77bcbd030c
ACAM023 <sonalpha023@gmail.com>
2019-03-16 21:46:24 +0300

    Adding message Function

--- db/db_libs.php
+++ db/db_libs.php
@@ -25,11 +25,28 @@ function selectAllChats($limit){
        $statement->execute();
 
        $chatList = $statement->fetchAll(PDO::FETCH_ASSOC);
-     }else {
-       //echo "Error (selectAllChats): ".$error->getMessage();
-     }
+     }/*else {
+       echo "Error (selectAllChats): ".$error->getMessage();
+     */}
 
    return $chatList;
+ }
+
+
+ function addToChats($limit){
+   global $conn, $error;
+
+   $chatAdd = null;
+   if ($conn != null){
+       $query = "INSERT  INTO chats (message, sent_time)";
+       $statement = $conn->prepare($query);
+       $statement->bindValue(':lim', 	$limit, 	PDO::PARAM_INT);
+       $statement->execute();
+
+       $chatAdd = $statement->fetchAll(PDO::FETCH_ASSOC);
+     }
+
+     return $chatAdd;
  }
 
 // TODO
